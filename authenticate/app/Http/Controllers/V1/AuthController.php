7c58763235627873e9b42ae02b80773593fce9a0
aghaee <a.aghaei@asiatech.cloud>
2024-12-14 19:08:03 +0330

    feat: add helper to format response

--- authenticate/app/Http/Controllers/V1/AuthController.php
+++ authenticate/app/Http/Controllers/V1/AuthController.php
@@ -2,20 +2,22 @@
 
 namespace App\Http\Controllers\V1;
 
+use App\Helpers\ApiResponse;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
+use Illuminate\Http\JsonResponse;
 
 class AuthController extends Controller
 {
-    public function login(LoginRequest $request)
+    public function login(LoginRequest $request): JsonResponse
     {
         $credentials = $request->only('phone_number', 'password');
 
         if (!$token = auth()->attempt($credentials)) {
-            return response()->json(['error' => 'Invalid phone number or password'], 401);
+            return ApiResponse::error(['message' => 'Invalid phone number or password'], 401);
         }
 
-        return response()->json([
+        return ApiResponse::success([
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth()->factory()->getTTL() * 60,
