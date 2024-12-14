df48f425e843138452bbff51dfe361f27a334658
aghaee <a.aghaei@asiatech.cloud>
2024-12-14 18:07:16 +0330

    feat: add new user to db seed

--- authenticate/database/seeders/DatabaseSeeder.php
+++ authenticate/database/seeders/DatabaseSeeder.php
@@ -13,11 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'amir aghaee',
+            'phone_number' => '09378239592',
+            'password' => 'amir123',
         ]);
     }
 }
