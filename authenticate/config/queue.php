83ef5f34b22057723c35ddde0baa88fd67a94df2
amiraghaee <amir.aghaee73@gmail.com>
2024-12-15 11:03:34 +0330

    feat: add docker compose

--- authenticate/config/queue.php
+++ authenticate/config/queue.php
@@ -76,7 +76,7 @@ return [
             'driver' => 'rabbitmq',
             'hosts' => [
                 [
-                    'host' => env('RABBITMQ_HOST', '127.0.0.1'),
+                    'host' => env('RABBITMQ_HOST', 'rabbitmq'),
                     'port' => env('RABBITMQ_PORT', 5672),
                     'user' => env('RABBITMQ_USER', 'guest'),
                     'password' => env('RABBITMQ_PASSWORD', 'guest'),
